193ea8b8c7f1f0b2d82a308337f84d2e8c963096
kayzee <olaniyikazeem112@gmail.com>
2022-08-18 21:16:37 +0100

    last commit

--- scripts/todo.ts
+++ scripts/todo.ts
@@ -15,4 +15,5 @@ main().catch((error) => {
     process.exitCode = 1;
   });
 
-  // 0xbf8C47baccB9c8410fcfcc2E6d052b7b8Dc4FE85
+  // Contract Address = 0xbf8C47baccB9c8410fcfcc2E6d052b7b8Dc4FE85
+  // Contract Transaction = 0x1af364c8037c4af01b12f5d6c23ce5e4933757fbd6b1ced4d958b23b4d3691bf
---
+++ scripts/todointeraction.ts
@@ -0,0 +1,20 @@
+import { ethers } from "hardhat";
+
+
+async function main() {
+    const Todo = await ethers.getContractAt("ToDoList", "0xbf8C47baccB9c8410fcfcc2E6d052b7b8Dc4FE85");
+    
+
+    //Interact
+
+    await Todo.AddTodo("Let me see");
+    await Todo.checkAllElementInTodo();
+    await Todo.deleteAllElementInTodo();
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
+
+  // TxHash = 0xf7093eb4f23264fa98a00a7407fc4f72ae9ea2902de7591a43be40a278b75b98
